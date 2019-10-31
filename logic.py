09dbcdbe91a1422aa4a329d724d1ea2736f04e13
eklekmek07 <38191058+eklekmek07@users.noreply.github.com>
2019-10-31 12:28:50 +0300

    dot.delete update
    +dot and line deleting
    +if a line does not have any dot, line will be deleted
    *line cross eachother

--- logic.py
+++ logic.py
@@ -22,7 +22,7 @@ class Main:
         
         self.addedDotPF = 2 #added dot per frame
         self.deletedDotPF = 1
-        self.startingDot = 1
+        self.startingDot = 20
         self.screenBorderLimit = 20 #set the avaliable zone for dot how far from the window border
         
         self.showRange = True  #for debuging purpose
@@ -31,8 +31,9 @@ class Main:
         self.dots = [] #list that store dots
         self.links = []
 
-        for i in range(self.startingDot):
-            self.createDot()
+        for i in range(10):
+            self.createDot(randint(30,80), 2)
+            self.randomDotSelector()
 
     def run(self): #mainloop
         clock = pygame.time.Clock()
@@ -44,26 +45,35 @@ class Main:
                     pygame.quit() 
                     quit()
             #####LOGIC######
-            self.linkCreator()
+            self.deleteDot()
             self.createDot(randint(30,80), 2)
-            #self.deleteDot()
+            self.createDot(randint(30,80), 2)
+            self.createDot(randint(30,80), 2)
+            self.randomDotSelector()
+            self.randomDotSelector()
+            self.randomDotSelector()
             #####RENDER#####
             self.screen.fill((255,255,255)) #this is important because every frame need fresh background
             self.renderLine()
             self.renderDots()
             self.displayText()
-            
+                                
             pygame.display.flip()
 
     def displayText(self): #make oop friendly
         font = pygame.font.Font(None, 36)
-        text = font.render(str(len(self.dots)), 1, (10, 10, 10))
+        text = font.render(str(len(self.dots)), 1, (10, 11, 10))
         textpos = text.get_rect()
         textpos.centerx = self.screen.get_rect().centerx
         self.screen.blit(text, textpos)
     
-    def linkCreator(self):
-        if len(self.dots) > 1:
+    def displayPos(self, pos):
+        font = pygame.font.Font(None, 36)
+        text = font.render(str(round(pos[0],round(pos[1]))), 1, (10, 10, 10))
+        self.screen.blit(text, pos)
+
+    def randomDotSelector(self):
+        if len(self.dots) > 1: #failsafe for not sellectable dot : out of index error
             fakeDots = self.dots.copy()
             dot1 = fakeDots.pop(randint(0, len(fakeDots)-1))
             dot2 = fakeDots.pop(randint(0, len(fakeDots)-1))
@@ -74,22 +84,21 @@ class Main:
         tempDot = Dot(pos, minRange, linkLimit, self.showRange)
         if self.canDotable(tempDot.pos, tempDot.minRange):
             self.dots.append(tempDot)
-    def createLink(self, dot1, dot2):
-        mainLink = Link(dot1, dot2)
-        for link in self.links:
-            if not self.canCreateLink(mainLink, link):
-                return False
-        tempLink = Link(dot1, dot2)
-        dot1.links.append(tempLink)
-        self.links.append(tempLink)
     
+    def createLink(self, dot1, dot2):
+        tempLink = Link(dot1, dot2)
+        for link in self.links:
+            if not self.linkIntersection(tempLink, link):
+                return False
+        dot1.links.append(tempLink)
+        dot2.links.append(tempLink)
+        self.links.append(tempLink)
+   
     def deleteDot(self): #can be changed to delete both lines and dots
         if len(self.dots) == 0: #failsafe in case there is not any dot to delete
             return False 
         self.dots[0].delete()
-    def checkLinks(self):
-        for link in self.links:
-            link.check()
+        del self.dots[0]
 
     def renderDots(self):
         for dot in self.dots:
@@ -105,14 +114,24 @@ class Main:
             distance = (((pos[x] - dot.pos[x])**2) + ((pos[y] - dot.pos[y])**2)) ** 0.5
             if distance < dot.minRange + minRange:
                 return False
-        return True    
-    def canCreateLink(self, link1, link2):
+        return True   
+ 
+    """def canCreateLink(self, link1, link2):
         D  = link1.A * link2.B - link1.B * link2.A
         if D != 0:
-            print("döner")
             return True
-        return False
+        return False"""
 
+    def linkIntersection(self, link1, link2):
+        D  = link1.A * link2.B - link1.B * link2.A
+        Dx = link1.C * link2.B - link1.B * link2.C
+        Dy = link1.A * link2.C - link1.C * link2.A
+        if D != 0:
+            x = Dx / D
+            y = Dy / D
+            return True
+        else:
+            return False
 
 class Dot:
     def __init__(self, pos, minRange, linkLimit, showRange = True):
@@ -135,31 +154,34 @@ class Dot:
 class Link:
     def __init__(self, dot1, dot2, color = BLUE):
         self.live = True #Link do not live if on or two of dots disappear and delete itself
-        
-        self.pos1 = dot1.pos
-        self.pos2 = dot2.pos
+        self.dot1 = dot1
+        self.dot2 = dot2
         self.connectedDots = []
         self.connectedDots.append(dot1)
         self.connectedDots.append(dot2)
-        
+        self.A = self.dot1.pos[y] - self.dot2.pos[y]
+        self.B = self.dot2.pos[x] - self.dot1.pos[x]
+        self.C = -((self.dot1.pos[x] * self.dot2.pos[y]) - (self.dot2.pos[x] * self.dot1.pos[y]))
         self.color = color
         self.width = None
-        
-        self.A = self.pos2[y] - self.pos1[y]
-        self.B = self.pos2[x] - self.pos1[x]
-        #self.C = -((pos1[x] * pos2[y]) - (pos2[x] * pos1[y])) in the future for kesişim..
-        self.distance = None
+        self.check()
 
-    def check(self): #not useful for now
-        self.pos1 = dot1.pos
-        self.pos2 = dot2.pos        
-        self.distance = None #diy calculate the distance between connected dots
-        if self.connectedDots >= 1:
+    def check(self): #not useful for now      
+        self.distance = (((self.dot1.pos[x] - self.dot2.pos[x])**2) + ((self.dot1.pos[y] - self.dot2.pos[y])**2))**0.5
+        if self.distance > 500:
+            self.color = RED
+        elif self.distance > 200:
+            self.color = BLUE
+        else:
+            self.color = GREEN
+        if len(self.connectedDots) <= 1:
             self.live = False    
-
+    
     def render(self, screen):
+        self.check()
+        print(self.live)
         if self.live:
-            pygame.draw.aaline(screen, self.color, self.pos1, self.pos2)
+            pygame.draw.aaline(screen, self.color, self.dot1.pos, self.dot2.pos)
 
 main = Main()
 main.run()
